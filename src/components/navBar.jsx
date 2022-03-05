2e2c3f354ea84854095281526fb4dbdbbd6fec6c
TreySchneider95 <tschneid95@gmail.com>
2022-03-05 13:00:58 -0700

    day 1

---
+++ src/components/navBar.jsx
@@ -0,0 +1,12 @@
+import './navBar.css'
+
+
+const NavBar = ()=>{
+    return(
+        <div className='navBar'>
+            <h2>This is going to be the nav bar</h2>
+            </div>
+    )
+}
+
+export default NavBar
